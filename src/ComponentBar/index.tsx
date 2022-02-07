85cb0429f1955eddac1171e975838eaea482c141
肖秋锋 <xiaoqiufeng@ydjdev.com>
2022-02-07 16:31:57 +0800

    feat: component bar page name

--- src/ComponentBar/index.tsx
+++ src/ComponentBar/index.tsx
@@ -1,7 +1,15 @@
+import Input from '@shared/input'
 import './index.css'
 
 const ComponentBar = () => {
-  return <div className="component-bar">ComponentBar</div>
+  return (
+    <div className="component-bar">
+      <div className="page-name-wrapper">
+        <label htmlFor="name">页面名称</label>
+        <Input style={{ flex: 1 }} id="name" />
+      </div>
+    </div>
+  )
 }
 
 export default ComponentBar
